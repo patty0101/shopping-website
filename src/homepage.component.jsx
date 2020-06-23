fd7d85447db2fd3813f2af0722b773ccde0f6911
qingruzhou <patty@MacBook-Pro.local>
2020-06-23 17:39:25 -0400

    homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+const HomePage = () =>{
+    <div className="homepage">
+        <div className="direcotry-menu"></div>
+        <div className='menu-item'>
+            <div className='content'>
+                <h1 className='title'></h1>
+                <span className='subtite'>SHOP NOW</span>
+            </div>
+        </div>
+    </div>
+}
+
+export default HomePage
